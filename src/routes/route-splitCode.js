c993500b6357ff5e76574f8d1f5a2d78caec4c65
jiangxtx <xiajiujiang@foxmail.com>
2017-08-01 19:17:07 +0800

    optimize dynamic router for code-spliting;

--- src/routes/route-splitCode.js
+++ src/routes/route-splitCode.js
@@ -9,7 +9,6 @@ import {
 import IndexHead from '../containers/home/Header'
 import IndexFooter from '../components/index_footer'
 
-
 const Roots = (props) => (
     <div>
         <IndexHead />
@@ -23,39 +22,11 @@ const Roots = (props) => (
  * 按需加载之后，我们需要让路由动态加载组件。将路由拆出来（因为路由庞大之后全部写在一起会很难看）。
  * @type {{path: string, indexRoute: {}}}
  */
-const rootRoute_bkup = {
-    path: '/',
-    component: Roots.default,
-    getComponent(nextState, callback) {
-        require.ensure([], require => {
-            callback(null, require('../containers/home/Home').default)
-        }, 'home')
-    },
-    childRoutes: [
-        {
-            path: 'train',
-            getComponent(nextState, callback) {
-                require.ensure([], require => {
-                    callback(null, require('../containers/EducationTrain.container').default)
-                }, 'train')
-            },
-        }, {
-            path: 'educ',
-            getComponent(nextState, callback) {
-                require.ensure([], require => {
-                    callback(null, require('../containers/StudentEducationStudy.container').default)
-                }, 'educ')
-            },
-        }
-
-    ]
-}
-
 const rootRoute = {
     childRoutes: [
         {
             path: '/',
-            component: Roots,
+            component: Roots,  // should NOT write 'Roots.default'.
             indexRoute: {
                 getComponent(nextState, callback) {
                     require.ensure([], require => {
@@ -65,27 +36,114 @@ const rootRoute = {
             },
             childRoutes: [
                 {
-                    path: 'home',
-                    getComponent(nextState, callback) {
-                        require.ensure([], require => {
-                            callback(null, require('../containers/home/Home').default)
-                        }, 'home')
+                    path: `train`,
+                    component: require('../containers/EducationTrain.container').default,
+                    indexRoute: {
+                        getComponent(nextState, callback) {
+                            require.ensure([], require => {
+                                callback(null, require('../containers/EducationTrainMain.container').default)
+                            }, 'train')
+                        },
                     },
+                    childRoutes: [
+                        {
+                            path: 'online',
+                            getComponent(nextState, callback) {
+                                require.ensure([], require => {
+                                    callback(null, require('../containers/EducationTrainOnline.container').default)
+                                }, 'trainOnline')
+                            },
+                        }, {
+                            path: `train/detail/:tid`,
+                            getComponent(nextState, callback) {
+                                require.ensure([], require => {
+                                    callback(null, require('../containers/EducationTrainDetail.container').default)
+                                }, 'trainDetail')
+                            },
+                        }
+                    ],
                 }, {
-                    path: 'train',
-                    getComponent(nextState, callback) {
-                        require.ensure([], require => {
-                            callback(null, require('../containers/EducationTrain.container').default)
-                        }, 'train')
+                    path: `educ`,
+                    component: require('../containers/StudentEducation.container').default,
+                    indexRoute: {
+                        getComponent(nextState, callback) {
+                            require.ensure([], require => {
+                                callback(null, require('../containers/StudentEducationMain.container').default)
+                            }, 'educ')
+                        },
                     },
+                    childRoutes: [
+                        {
+                            path: 'study',
+                            getComponent(nextState, callback) {
+                                require.ensure([], require => {
+                                    callback(null, require('../containers/StudentEducationStudy.container').default)
+                                }, 'educStudy')
+                            },
+                        }
+                    ],
                 }, {
-                    path: 'educ',
-                    getComponent(nextState, callback) {
-                        require.ensure([], require => {
-                            callback(null, require('../containers/StudentEducation.container').default)
-                        }, 'educ')
+                    path: `case`,
+                    component: require('../containers/SuccessCase.container').default,
+                    indexRoute: {
+                        getComponent(nextState, callback) {
+                            require.ensure([], require => {
+                                callback(null, require('../containers/SuccessCaseMain.container').default)
+                            }, 'case')
+                        },
                     },
-                }
+                    childRoutes: [
+                        {
+                            path: `detail/:cid`,
+                            getComponent(nextState, callback) {
+                                require.ensure([], require => {
+                                    callback(null, require('../containers/SuccessCaseDetail.container').default)
+                                }, 'caseDetail')
+                            },
+                        }
+                    ],
+                }, {
+                    path: `about`,
+                    component: require('../containers/AboutUs.container').default,
+                    indexRoute: {
+                        getComponent(nextState, callback) {
+                            require.ensure([], require => {
+                                callback(null, require('../containers/AboutUsMain.container').default)
+                            }, 'about')
+                        },
+                    },
+                    childRoutes: [
+                        {
+                            path: `team`,
+                            getComponent(nextState, callback) {
+                                require.ensure([], require => {
+                                    callback(null, require('../containers/AboutUsTeam.container').default)
+                                }, 'aboutTeam')
+                            },
+                        }, {
+                            path: `partner`,
+                            getComponent(nextState, callback) {
+                                require.ensure([], require => {
+                                    callback(null, require('../containers/AboutUsPartner.container').default)
+                                }, 'aboutPartner')
+                            },
+                        }, {
+                            path: `recruit`,
+                            getComponent(nextState, callback) {
+                                require.ensure([], require => {
+                                    callback(null, require('../containers/AboutUsRecruit.container').default)
+                                }, 'aboutRecruit')
+                            },
+                        }, {
+                            path: `contact`,
+                            getComponent(nextState, callback) {
+                                require.ensure([], require => {
+                                    callback(null, require('../containers/AboutUsContact.container').default)
+                                }, 'aboutContact')
+                            },
+                        },
+                    ],
+                },
             ]
         }
     ]
